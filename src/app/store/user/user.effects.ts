8f1d59b9594252fed3bcf0b4eae1e0fa1ed65931
Max Tuzenko <max.tuzenko@gmail.com>
2023-04-15 21:16:31 +0200

    Configure user effects

--- src/app/store/user/user.effects.ts
+++ src/app/store/user/user.effects.ts
@@ -1,138 +1,96 @@
 import { Injectable } from '@angular/core';
-import { Effect, Actions, ofType } from '@ngrx/effects';
+import { createEffect, Actions, ofType } from '@ngrx/effects';
 import { Router } from '@angular/router';
 
-import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { firestore } from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 import { Observable, from, of } from 'rxjs';
 import { map, switchMap, catchError, take, tap, withLatestFrom } from 'rxjs/operators';
-
-import { environment } from '@src/environments/environment';
 
 import { User } from './user.models';
 
 import * as fromActions from './user.actions';
 
 import { NotificationService } from '@app/services';
+import firebase from 'firebase/compat/app';
 
-type Action = fromActions.All;
 
 @Injectable()
 export class UserEffects {
     constructor(
-        private actions: Actions,
+        private actions$: Actions,
         private afAuth: AngularFireAuth,
-        private afs: AngularFirestore,
-        private router: Router,
-        private notification: NotificationService
     ) { }
 
-    @Effect()
-    init: Observable<Action> = this.actions.pipe(
-        ofType(fromActions.Types.INIT),
-        switchMap(() => this.afAuth.authState.pipe(take(1))),
-        switchMap(authState => {
-            if (authState) {
-
-                return this.afs.doc<User>(`users/${authState.uid}`).valueChanges().pipe(
-                    take(1),
-                    map(user => new fromActions.InitAuthorized(authState.uid, user || null)),
-                    catchError(err => of(new fromActions.InitError(err.message)))
+    signInUserEmail$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(fromActions.userSignInEmail),
+            map((action) => action.credentials),
+            switchMap((credentials) => {
+                return from(this.afAuth.signInWithEmailAndPassword(credentials.email, credentials.password)).pipe(
+                    map((res) => {
+                        const user = res.user;
+                        return fromActions.userSignInEmailSuccess({ uid: user.uid, user });
+                    }),
+                    catchError((error) => {
+                        return of(fromActions.userSignInEmailError({ error: error.message }));
+                    })
                 );
-
-            } else {
-                return of(new fromActions.InitUnauthorized());
-            }
-        })
-    );
-
-    @Effect()
-    signInEmail: Observable<Action> = this.actions.pipe(
-        ofType(fromActions.Types.SIGN_IN_EMAIL),
-        map((action: fromActions.SignInEmail) => action.credentials),
-        switchMap(credentials =>
-            from(this.afAuth.auth.signInWithEmailAndPassword(credentials.email, credentials.password)).pipe(
-                switchMap(signInState =>
-                    this.afs.doc<User>(`users/${signInState.user.uid}`).valueChanges().pipe(
-                        take(1),
-                        tap(() => {
-                            this.router.navigate(['/']);
-                        }),
-                        map(user => new fromActions.SignInEmailSuccess(signInState.user.uid, user || null))
-                    )
-                ),
-                catchError(err => {
-                    this.notification.error(err.message);
-                    return of(new fromActions.SignInEmailError(err.message));
-                })
-            )
+            })
         )
     );
 
-    @Effect()
-    signUpEmail: Observable<Action> = this.actions.pipe(
-        ofType(fromActions.Types.SIGN_UP_EMAIL),
-        map((action: fromActions.SignUpEmail) => action.credentials),
-        switchMap(credentials =>
-            from(this.afAuth.auth.createUserWithEmailAndPassword(credentials.email, credentials.password)).pipe(
-                tap(() => {
-                    this.afAuth.auth.currentUser.sendEmailVerification(
-                        environment.firebase.actionCodeSettings
-                    );
-                    this.router.navigate(['/auth/email-confirm']);
-                }),
-                map((signUpState) => new fromActions.SignUpEmailSuccess(signUpState.user.uid)),
-                catchError(err => {
-                    this.notification.error(err.message);
-                    return of(new fromActions.SignUpEmailError(err.message));
-                })
-            )
+    signInUserGoogle$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(fromActions.userSignInWithGoogle),
+            switchMap((action) => {
+                return from(this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider())).pipe(
+                    map((credential) => {
+                        const user = credential.user;
+                        return fromActions.userSignInWithGoogleSuccess({ uid: user.uid, user });
+                    }),
+                    catchError((error) => {
+                        return of(fromActions.userSignInWithGoogleError({ error: error.message }));
+                    })
+                );
+            })
         )
     );
 
-    @Effect()
-    signOut: Observable<Action> = this.actions.pipe(
-        ofType(fromActions.Types.SIGN_OUT),
-        switchMap(() =>
-            from(this.afAuth.auth.signOut()).pipe(
-                map(() => new fromActions.SignOutSuccess()),
-                catchError(err => of(new fromActions.SignOutError(err.message)))
-            )
+    userSignOut$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(fromActions.userSignOut),
+            switchMap(() => {
+                return from(this.afAuth.signOut()).pipe(
+                    map(() => {
+                        return fromActions.userSignOutSuccess();
+                    }),
+                    catchError((error) => {
+                        return of(fromActions.userSignOutError({ error: error.message }));
+                    })
+                );
+            })
         )
     );
 
-    @Effect()
-    create: Observable<Action> = this.actions.pipe(
-        ofType(fromActions.Types.CREATE),
-        map((action: fromActions.Create) => action.user),
-        withLatestFrom(this.afAuth.authState.pipe(take(1))),
-        map(([user, state]) => ({
-            ...user,
-            uid: state.uid,
-            email: state.email,
-            created: firestore.FieldValue.serverTimestamp()
-        })),
-        switchMap((user: User) =>
-            from(this.afs.collection('users').doc(user.uid).set(user)).pipe(
-                tap(() => this.router.navigate(['/profile', user.uid])),
-                map(() => new fromActions.CreateSuccess(user)),
-                catchError(err => of(new fromActions.CreateError(err.message)))
-            )
-        )
-    );
+    // @Effect()
+    // init: Observable<Action> = this.actions.pipe(
+    //     ofType(fromActions.Types.INIT),
+    //     switchMap(() => this.afAuth.authState.pipe(take(1))),
+    //     switchMap(authState => {
+    //         if (authState) {
 
-    @Effect()
-    update: Observable<Action> = this.actions.pipe(
-        ofType(fromActions.Types.UPDATE),
-        map((action: fromActions.Update) => action.user),
-        switchMap(user =>
-            from(this.afs.collection('users').doc(user.uid).set(user)).pipe(
-                tap(() => this.router.navigate(['/profile', user.uid])),
-                map(() => new fromActions.UpdateSuccess(user)),
-                catchError(err => of(new fromActions.UpdateError(err.message)))
-            )
-        )
-    );
+    //             return this.afs.doc<User>(`users/${authState.uid}`).valueChanges().pipe(
+    //                 take(1),
+    //                 map(user => new fromActions.InitAuthorized(authState.uid, user || null)),
+    //                 catchError(err => of(new fromActions.InitError(err.message)))
+    //             );
+
+    //         } else {
+    //             return of(new fromActions.InitUnauthorized());
+    //         }
+    //     })
+    // );
+
 }
