62a1680ff39764b58bc4aa9dcb59d6f9dee3825b
Max Tuzenko <max.tuzenko@gmail.com>
2023-04-16 10:57:45 +0200

    Add user reducers

--- src/app/store/user/user.reducer.ts
+++ src/app/store/user/user.reducer.ts
@@ -1,5 +1,6 @@
 import { User } from './user.models';
 import * as fromActions from './user.actions';
+import { createReducer, on } from '@ngrx/store';
 
 export interface UserState {
     entity: User;
@@ -14,101 +15,33 @@ const initialState: UserState = {
     loading: null,
     error: null
 };
+export const reducer = createReducer(
+    initialState,
 
-export function reducer(state = initialState, action: fromActions.All): UserState {
-    switch (action.type) {
+    // Init
+    on(fromActions.userInit, (state) => ({ ...state, loading: true })),
+    on(fromActions.userInitAuthorized, (state, { uid, user }) => ({ ...state, entity: user, uid: uid, loading: false, error: null })),
+    on(fromActions.userInitUnauthorized, (state) => ({ ...state, entity: null, loading: false, error: null })),
+    on(fromActions.userInitError, (state, { error }) => ({ ...state, loading: false, error })),
 
-        // Init
+    // Sign In Email
+    on(fromActions.userSignInEmail, (state) => ({ ...state, loading: true })),
+    on(fromActions.userSignInEmailSuccess, (state, { uid, user }) => ({ ...state, entity: user, uid, loading: false, error: null })),
+    on(fromActions.userSignInEmailError, (state, { error }) => ({ ...state, loading: false, error })),
 
-        case fromActions.Types.INIT: {
-            return { ...state, loading: true };
-        }
+    // Sign In Google
+    on(fromActions.userSignInWithGoogle, (state) => ({ ...state, loading: true })),
+    on(fromActions.userSignInWithGoogleSuccess, (state, { uid, user }) => ({ ...state, entity: user, uid, loading: false, error: null })),
+    on(fromActions.userSignInWithGoogleError, (state, { error }) => ({ ...state, loading: false, error })),
 
-        case fromActions.Types.INIT_AUTHORIZED: {
-            return { ...state, entity: action.user, uid: action.uid, loading: false, error: null };
-        }
+    // // Sign Up
+    // on(fromActions.userSignUpEmail, (state) => ({ ...state, loading: true })),
+    // on(fromActions.userSignUpEmailSuccess, (state, { uid, user }) => ({ ...state, entity: user, uid, loading: false, error: null })),
+    // on(fromActions.userSignUpEmailError, (state, { error }) => ({ ...state, loading: false, error })),
 
-        case fromActions.Types.INIT_UNAUTHORIZED: {
-            return { ...state, entity: null, loading: false, error: null };
-        }
+    // Sign Out
+    on(fromActions.userSignOut, (state) => ({ ...state, loading: true })),
+    on(fromActions.userSignOutSuccess, () => ({ ...initialState })),
+    on(fromActions.userSignOutError, (state, { error }) => ({ ...state, loading: false, error })),
 
-        case fromActions.Types.INIT_ERROR: {
-            return { ...state, loading: false, error: action.error };
-        }
-
-
-        // Sign In
-
-        case fromActions.Types.SIGN_IN_EMAIL: {
-            return { ...state, loading: true };
-        }
-
-        case fromActions.Types.SIGN_IN_EMAIL_SUCCESS: {
-            return { ...state, entity: action.user, uid: action.uid, loading: false, error: null };
-        }
-
-        case fromActions.Types.SIGN_IN_EMAIL_ERROR: {
-            return { ...state, error: action.error, loading: false };
-        }
-
-        // Sign Up
-
-        case fromActions.Types.SIGN_UP_EMAIL: {
-            return { ...state, loading: true };
-        }
-
-        case fromActions.Types.SIGN_UP_EMAIL_SUCCESS: {
-            return { ...state, uid: action.uid, loading: false };
-        }
-
-        case fromActions.Types.SIGN_UP_EMAIL_ERROR: {
-            return { ...state, error: action.error, loading: false };
-        }
-
-        // Sign Out
-
-        case fromActions.Types.SIGN_OUT: {
-            return { ...state, loading: true };
-        }
-
-        case fromActions.Types.SIGN_OUT_SUCCESS: {
-            return { ...initialState };
-        }
-
-        case fromActions.Types.SIGN_OUT_ERROR: {
-            return { ...state, error: action.error, loading: false };
-        }
-
-        // Create
-
-        case fromActions.Types.CREATE: {
-            return { ...state, loading: true, error: null };
-        }
-
-        case fromActions.Types.CREATE_SUCCESS: {
-            return { ...state, entity: action.user, loading: false };
-        }
-
-        case fromActions.Types.CREATE_ERROR: {
-            return { ...state, loading: false, error: action.error };
-        }
-
-        // Update
-
-        case fromActions.Types.UPDATE: {
-            return { ...state, loading: true, error: null };
-        }
-
-        case fromActions.Types.UPDATE_SUCCESS: {
-            return { ...state, entity: action.user, loading: false };
-        }
-
-        case fromActions.Types.UPDATE_ERROR: {
-            return { ...state, loading: false, error: action.error };
-        }
-
-        default: {
-            return state;
-        }
-    }
-}
+);
