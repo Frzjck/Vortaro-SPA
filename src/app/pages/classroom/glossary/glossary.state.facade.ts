4e9e1bedf8e0f71712555a1089a7786ecf4e7add
Max Tuzenko <max.tuzenko@gmail.com>
2023-04-28 18:05:14 +0200

    Add wordGrid and glossary views to facade and state

--- src/app/pages/classroom/glossary/glossary.state.facade.ts
+++ src/app/pages/classroom/glossary/glossary.state.facade.ts
@@ -2,12 +2,24 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { GlossaryState } from './glossary.state';
+import { Observable } from 'rxjs';
+import { WordGridViewInterface } from './components/word-grid/word-grid.component';
+import { GlossaryViewInterface } from './glossary.component';
 
 
 
 @Injectable(
-    { providedIn: 'root' }
 )
 export class GlossaryStateFacade {
     constructor(private store: Store, private glossaryState: GlossaryState) { }
+
+    // ViewModels and Interfaces:
+    wordGridView$(): Observable<WordGridViewInterface> {
+        return this.glossaryState.wordGridView$;
+    }
+
+    glossaryView$(): Observable<GlossaryViewInterface> {
+        return this.glossaryState.glossaryView$;
+    }
+
 }
--- src/app/pages/classroom/glossary/glossary.state.ts
+++ src/app/pages/classroom/glossary/glossary.state.ts
@@ -1,18 +1,29 @@
-import { ComponentStore, tapResponse } from '@ngrx/component-store';
+import { ComponentStore } from '@ngrx/component-store';
 import { Injectable } from '@angular/core';
-import { Word, getWords, getWordsByGroupId } from '@app/pages/classroom/store/words-list';
+import { getWordsByGroupId } from '@app/pages/classroom/store/words-list';
 import { Group, getGroups } from '@app/pages/classroom/store/groups-list';
 import { Store } from '@ngrx/store';
-import { Observable, exhaustMap, map, tap } from 'rxjs';
+import { Observable, map } from 'rxjs';
+import { WordGridViewInterface } from './components/word-grid/word-grid.component';
+import { GlossaryViewInterface } from './glossary.component';
 
 export interface GlossaryStateModel {
     unfoldedWords: string[];
+
+
     editingGroupId: string;
+    editingGroupNameId: string;
+    editingWordId: string;
+    addNewWordMode: boolean;
 }
 
 const initialState = {
     unfoldedWords: [],
+
     editingGroupId: null,
+    editingGroupNameId: null,
+    editingWordId: null,
+    addNewWordMode: false,
 };
 
 @Injectable()
@@ -21,10 +32,68 @@ export class GlossaryState extends ComponentStore<GlossaryStateModel> {
     constructor(private store: Store) {
         super(initialState);
     }
+    // State selectors:
+    readonly unfoldedWords$ = this.select(state => state.unfoldedWords);
+
+    readonly editingGroupId$ = this.select(state => state.editingGroupId);
+    readonly editingWordId$ = this.select(state => state.editingWordId);
+    readonly isAddingNewWord$ = this.select(state => state.addNewWordMode);
+
+    // Global state selectors:
+    readonly groupsAndWordsObs$ = this.store.select(getGroups)
+        .pipe(
+            map((groups: Group[]) => {
+                return groups.map((group) => {
+                    return {
+                        group,
+                        words$: this.store.select(getWordsByGroupId(group.id))
+                    }
+                })
+            })
+        )
 
 
-    // EditingGroup
-    editingGroupId$ = this.select(state => state.editingGroupId);
+    // Combined selectors:
+    readonly isAllFolded$ = this.select(state => state.unfoldedWords.length === 0);
+
+    readonly isEditingWord$ = this.select(state => !!state.editingWordId);
+    readonly isEditingGroup$ = this.select(state => !!state.editingGroupId);
+    readonly isEditingGroupName$ = this.select(state => !!state.editingGroupNameId);
+
+    readonly groupsAndWords$ = this.select(
+        this.groupsAndWordsObs$,
+        // this.store.select(getGroups),
+        (groupsAndWordsObs) => {
+            return groupsAndWordsObs;
+        }
+    )
+
+    // ViewModels and Interfaces:
+    readonly wordGridView$: Observable<WordGridViewInterface> = this.select(
+        this.editingGroupId$,
+        this.isEditingGroup$,
+        this.isAddingNewWord$,
+        (editingGroupId, isEditingGroup, isAddingNewWord) => ({
+            isEditingGroup,
+            editingGroupId,
+            isAddingNewWord,
+        })
+    );
+    // : Observable<GlossaryViewInterface>
+
+    readonly glossaryView$: Observable<GlossaryViewInterface> = this.select(
+        this.groupsAndWords$,
+        (groupsAndWords) => ({
+            groupsAndWords: groupsAndWords,
+        })
+    );
+
+
+
+    // Updaters:
+
+    // Effects:
+
 
     readonly setEditingGroup = (groupId: string) => this.patchState({ editingGroupId: groupId });
     readonly resetEditingGroup = this.updater(state => ({
@@ -32,15 +101,19 @@ export class GlossaryState extends ComponentStore<GlossaryStateModel> {
         editingGroupId: initialState.editingGroupId,
     }));
 
+    // readonly wordUiInterface = this.select(state => state.editingGroupId)
+
+
+
 
     // UnfoldedWords
-    readonly unfoldedWords$ = this.select(state => state.unfoldedWords);
-    readonly isAllFolded$ = this.select(state => state.unfoldedWords.length === 0);
+
 
     readonly foldAllWords = this.updater(state => ({
         ...state,
         unfoldedWords: initialState.unfoldedWords,
     }));
+
     readonly foldWord = this.updater((state, wordId: string) => ({
         ...state,
         unfoldedWords: state.unfoldedWords.filter((id) => id !== wordId),
@@ -70,25 +143,7 @@ export class GlossaryState extends ComponentStore<GlossaryStateModel> {
     // );
 
     // GroupsAndWords
-    readonly groupsAndWordsObs$ = this.store.select(getGroups)
-        .pipe(
-            map((groups: Group[]) => {
-                return groups.map((group) => {
-                    return {
-                        group,
-                        words$: this.store.select(getWordsByGroupId(group.id))
-                    }
-                })
-            })
-        )
 
-    readonly groupsAndWords$ = this.select(
-        this.groupsAndWordsObs$,
-        // this.store.select(getGroups),
-        (groupsAndWordsObs) => {
-            return groupsAndWordsObs;
-        }
-    )
 
 
 }
