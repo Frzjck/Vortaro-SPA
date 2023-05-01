d9ea7d067d3e831e5b02117b18b7cab0f4741cf7
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-01 18:41:52 +0200

    Create exercises component
    Presentational facade/container for exercises quiz and spelling

---
+++ src/app/pages/classroom/exercises/pages/exercises/exercises.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-exercises',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './exercises.component.html',
+  styleUrls: ['./exercises.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ExercisesComponent {
+
+}
