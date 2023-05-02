6e781d22de9ae52b5ce31204e4d9387abc17587b
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-02 19:44:55 +0200

    Move template and styles inside exercise container

--- src/app/pages/classroom/exercises/pages/exercises/exercise-container.component.ts
+++ src/app/pages/classroom/exercises/pages/exercises/exercise-container.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { QuizComponent } from './pages/quiz/quiz.component';
-import { SpellingComponent } from './pages/spelling/spelling.component';
-import { RouterModule } from '@angular/router';
+import { QuizComponent } from './pages/modes/quiz/quiz.component';
+import { SpellingComponent } from './pages/modes/spelling/spelling.component';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-exercises',
   standalone: true,
   imports: [CommonModule, QuizComponent, SpellingComponent, RouterModule],
-  templateUrl: './exercise-container.component.html',
-  styleUrls: ['./exercise-container.component.scss'],
+  template: `
+  <h1>exercises works!</h1>
+  <img
+    src="https://www.giantfreakinrobot.com/wp-content/uploads/2022/06/hellotherethumb.jpg"
+    alt=""
+    style="height: 15rem"
+  />
+  <h2>router outlet ↓</h2>
+  <router-outlet></router-outlet>
+  <h2>router outlet ↑</h2>
+`,
+  styles: [],
 })
 export class ExerciseContainerComponent {
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    // if selected quiz redirect to quiz component
+    // this.router.navigate(['results'], { relativeTo: this.route });
+    // if selected spelling redirect to spelling component
+
+  }
 
 }
