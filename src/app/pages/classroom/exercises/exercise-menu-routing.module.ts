8a9e1053ebf2279f76dc35216970541778041573
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-04 10:55:48 +0200

    Provide exercise state on exercise menu level

--- src/app/pages/classroom/exercises/exercise-menu-routing.module.ts
+++ src/app/pages/classroom/exercises/exercise-menu-routing.module.ts
@@ -3,11 +3,20 @@ import { Route } from '@angular/router';
 import { ExerciseContainerComponent } from './pages/exercises/exercise-container.component';
 import { ExerciseMenuComponent } from './exercise-menu.component';
 import { SelectGroupComponent } from './pages/select-group/select-group.component';
+import { importProvidersFrom } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { exercisesFeatureKey } from './store';
+import { exercisesReducer } from './store/exercises/exercises.reducers';
 
 export default [
   {
     path: '',
     component: ExerciseMenuComponent,
+    providers: [
+      importProvidersFrom(
+        StoreModule.forFeature(exercisesFeatureKey, exercisesReducer)
+      )
+    ],
   },
   {
     path: 'exercises/:exerciseType/:groupId',
