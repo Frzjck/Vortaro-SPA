ad1c0622cbff5c68f37526eaeb15570f74e3b76e
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-04 10:58:24 +0200

    Working exercise container

--- src/app/pages/classroom/exercises/pages/exercises/exercise-container.component.ts
+++ src/app/pages/classroom/exercises/pages/exercises/exercise-container.component.ts
@@ -4,19 +4,22 @@ import { QuizComponent } from './components/modes/quiz/quiz.component';
 import { SpellingComponent } from './components/modes/spelling/spelling.component';
 import { RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { getCurrentRoute } from '@app/store/router/router.selector';
+import { getParams } from '@app/store/router/router.selector';
 import { ResultsComponent } from './components/results/results.component';
+import { map } from 'rxjs';
+import { getExerciseMode, getExerciseStatus } from '@exercises/store/exercises';
 
 @Component({
   selector: 'app-exercises',
   standalone: true,
   imports: [CommonModule, QuizComponent, SpellingComponent, RouterModule, ResultsComponent],
   template: `
-    <ng-container [ngSwitch]="content">
+    <ng-container *ngIf="(exerciseStatus$ | async) === 'start'" [ngSwitch]="exerciseMode$ | async">
       <app-spelling *ngSwitchCase="'spelling'" />
       <app-quiz *ngSwitchCase="'quiz'" />
-      <app-results *ngSwitchCase="'results'" />
     </ng-container>
+
+    <app-results *ngIf="(exerciseStatus$ | async) === 'results'" />
 `,
   styles: [],
 })
@@ -24,17 +27,11 @@ export class ExerciseContainerComponent {
 
   constructor(private store: Store) { }
 
-  public content = 'results';
+  public exerciseMode$ = this.store.select(getExerciseMode);
+  public exerciseStatus$ = this.store.select(getExerciseStatus)
 
   ngOnInit(): void {
-    // if selected quiz redirect to quiz component
-    // this.router.navigate(['results'], { relativeTo: this.route });
-    // if selected spelling redirect to spelling component
-    this.store.select(getCurrentRoute).subscribe((router) => {
-      console.log("ROUTER", router);
-      // console.log("ROUTER", router.urlSegment?[0].path);
-    });
-
+    this.exerciseMode$.subscribe(console.log)
   }
 
 }
