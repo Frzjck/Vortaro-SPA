ac45e62ee81d80b786ba1e6393a07dda1165734c
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-04 10:54:50 +0200

    Create custom rxjs operator switchCase

---
+++ src/app/pages/classroom/exercises/pages/exercises/utils/switchCase.ts
@@ -0,0 +1,35 @@
+import { Observable, OperatorFunction, iif, defer, from } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+type ConditionFunction<T, R> = (value: T) => boolean;
+type CaseFunction<T, R> = (value: T) => Observable<R>;
+
+export function switchCase<T, R>(
+    ...cases: [ConditionFunction<T, boolean>, CaseFunction<T, R>][]
+): OperatorFunction<T, R> {
+    return (source: Observable<T>): Observable<R> => {
+        return source.pipe(
+            switchMap((value: T) => {
+                const matchedCase = cases.find(([condition]) => condition(value));
+                if (matchedCase) {
+                    const [, caseFn] = matchedCase;
+                    return defer(() => caseFn(value));
+                }
+                return from([]);
+            })
+        );
+    };
+}
+
+// Implementation example:
+// const source$ = of(2, 3, 4, 5, 6);
+
+// const result$ = source$.pipe(
+//   switchCase(
+//     [(value: number) => value === 2, () => of('two')],
+//     [(value: number) => value === 3, () => of('three')],
+//     [(value: number) => value === 4, () => of('four')],
+//     [(value: number) => value === 5, () => of('five')],
+//     [(value: number) => value === 6, () => of('six')]
+//   )
+// );
