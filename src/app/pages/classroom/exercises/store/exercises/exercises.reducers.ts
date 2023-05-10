ca31a4566d6015a07badf5ba0d76ac68db44b765
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-10 11:02:17 +0200

    Add SubmitButtonActionType and TestingAgainstType

--- src/app/pages/classroom/exercises/store/exercises/exercises.reducers.ts
+++ src/app/pages/classroom/exercises/store/exercises/exercises.reducers.ts
@@ -1,12 +1,21 @@
 import { createFeature, createReducer, createSelector, on } from '@ngrx/store';
 import * as userActions from '@app/store/user/user.actions';
 import { Word, selectWordEntities, selectWordsByGroupId } from '@app/pages/classroom/store/words-list';
-import { ExerciseContainerPageAPI, ExerciseContainerPageAction } from './exercises.actions';
+import { ExerciseContainerPageAPI, ExerciseContainerPageAction, ExercisePageAction } from './exercises.actions';
 import { getParams } from '@app/store/router/router.selector';
 import { selectWords } from '@app/pages/classroom/store/words-list';
 import { shuffle } from '../../pages/exercises/utils/shuffleArray';
 import { selectGroupEntities } from '@app/pages/classroom/store/groups-list';
 
+export enum SubmitButtonActionType {
+    PROOFREAD = "proofread",
+    NEXT = "next"
+};
+
+export enum TestingAgainstType {
+    ORIGINAL = "original",
+    TRANSLATION = "translation"
+};
 
 export interface ExercisesState {
     exerciseMode: string;
@@ -17,11 +26,11 @@ export interface ExercisesState {
 
     // This option determines what is being tested, your knowledge of meaning of a foreign word if true,
     // and your knowledge of spelling if false
-    translateDirection: boolean;
+    testingAgainst: TestingAgainstType;
 
     activeWordIndex: number;
     correct: boolean;
-    submitButtonAction: "proofread" | "next";
+    submitButtonAction: SubmitButtonActionType;
 
     // results functionality
     resultScores: boolean[]
@@ -38,12 +47,12 @@ export const initialState: ExercisesState = {
 
     exerciseWords: [],
 
-    translateDirection: true,
+    testingAgainst: TestingAgainstType.TRANSLATION,
 
     activeWordIndex: 0,
     resultScores: [],
     correct: null,
-    submitButtonAction: "proofread",
+    submitButtonAction: SubmitButtonActionType.PROOFREAD,
 
     wordWorthPercent: null,
 };
@@ -56,8 +65,9 @@ export const exercisesFeature = createFeature({
         on(ExerciseContainerPageAction.enter, (state) => ({ ...state, randomSeed: Math.random() })),
         on(ExerciseContainerPageAction.resetExerciseState, () => ({ ...initialState })),
         on(ExerciseContainerPageAPI.storeExerciseWords, (state, { exerciseWords }) => ({ ...state, exerciseWords: exerciseWords })),
-
+        on(ExercisePageAction.saveAnswer, (state, { answer }) => ({ ...state, resultScores: [...state.resultScores, answer] }))
     ),
+
     extraSelectors: ({ selectRandomSeed, selectActiveWordIndex, selectExerciseWords }) => ({
         selectCurrentGroupExerciseWords: createSelector(
             getParams,
@@ -99,7 +109,7 @@ export const {
     name, // feature name
     reducer, // feature reducer
     selectExercisesState, // feature selector
-    selectTranslateDirection,
+    selectTestingAgainst,
     selectExerciseMode,
     selectExerciseStatus,
     selectRandomSeed,
