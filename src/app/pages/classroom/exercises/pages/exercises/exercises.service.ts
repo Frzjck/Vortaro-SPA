777b0d24eda8e2c271fc2e63cf1b440eae22c514
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-10 22:48:53 +0200

    Finish setting up onSubmitAction in exercises.service

--- src/app/pages/classroom/exercises/pages/exercises/exercises.service.ts
+++ src/app/pages/classroom/exercises/pages/exercises/exercises.service.ts
@@ -6,33 +6,41 @@ import { Word } from '@app/pages/classroom/store/words-list';
 @Injectable({
   providedIn: 'root'
 })
-export class ExercisesService {
+export class ExerciseService {
 
   constructor(private store: Store) { }
 
   async onSubmitAction(word, inputValue): Promise<void> {
     const submitButtonAction = await firstValueFrom(this.store.select(selectSubmitButtonAction));
+    //toggle submit action
+    this.store.dispatch(ExercisePageAction.submitButtonActionToggle());
 
     if (submitButtonAction === SubmitButtonActionType.NEXT) {
       //if action is "next" -> dispatch next word(update index) and reset last correct answer. if no more words -> dispatch results (ondestroy results reset store)
+      this.store.dispatch(ExercisePageAction.nextWord());
       return;
     }
 
-    const isCorrect = await this.isResponseCorrect(word, inputValue);
-    this.store.dispatch(ExercisePageAction.saveAnswer({ answer: isCorrect }))
     // else (action is proofread)
     //check if solution is correct
+    const isCorrect = await this.isResponseCorrect(word, inputValue);
     //dispatch resultScores update to store result
     //dispatch correct answer to store so it can be highlighted
-    //change submit action to "next"
+    this.store.dispatch(ExercisePageAction.saveAnswer({ answer: isCorrect }));
+
   }
 
   async isResponseCorrect(word: Word, response): Promise<boolean> {
     const testingAgainst = await firstValueFrom(this.store.select(selectTestingAgainst));
 
-    if (testingAgainst === TestingAgainstType.TRANSLATION) return word.translation === response;
+    if (testingAgainst === TestingAgainstType.TRANSLATION) {
+      const possibleAnswers = [word.translation.toLowerCase()]
 
-    else if (testingAgainst === TestingAgainstType.ORIGINAL) return word.original === response;
+      if (word.additionalTr?.length) possibleAnswers.push(...word.additionalTr.map((x) => x.toLocaleLowerCase()))
+
+      return possibleAnswers.includes(response.toLowerCase());
+    }
+    else if (testingAgainst === TestingAgainstType.ORIGINAL) return word.original.toLowerCase() === response.toLowerCase();
   }
 
 }
