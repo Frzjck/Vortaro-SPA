8a9c47ef4b417973ee92289458823084e6b45ab1
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-17 11:45:43 +0200

    Create changeExerciseMode reducer

--- src/app/store/app/app.reducer.ts
+++ src/app/store/app/app.reducer.ts
@@ -51,5 +51,5 @@ export const reducer = createReducer(
         else newTestAgainst = TestingAgainstType.TRANSLATION;
         return { ...state, baseTestingAgainst: newTestAgainst };
     }),
-    on(SettingsPopupAction.changeTheme, (state, payload) => ({ ...state, ...payload })),
+    on(SettingsPopupAction.changeTheme, SettingsPopupAction.changeExerciseMode, (state, payload) => ({ ...state, ...payload })),
 );
