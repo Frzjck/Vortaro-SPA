51a9f28cb9c34ab54ced7eeed6af43b0a7fe0639
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-17 11:43:59 +0200

    Create toggleTestingAgainst reducer

--- src/app/store/app/app.reducer.ts
+++ src/app/store/app/app.reducer.ts
@@ -1,5 +1,6 @@
 import { createReducer, on } from '@ngrx/store';
 import { changeTheme, togglePixies } from './app.actions';
+import { SettingsPopupAction } from '@app/components/navbar/components/settings-popup/settings-popup.actions';
 
 
 export enum ThemeType {
@@ -44,6 +45,11 @@ export const initialState: AppState = {
 export const reducer = createReducer(
     initialState,
     on(SettingsPopupAction.togglePixies, (state) => ({ ...state, pixies: !state.pixies })),
-    on(changeTheme, (state, payload) => ({ ...state, ...payload })),
+    on(SettingsPopupAction.toggleTestingAgainst, (state) => {
+        let newTestAgainst;
+        if (state.baseTestingAgainst === TestingAgainstType.TRANSLATION) newTestAgainst = TestingAgainstType.ORIGINAL;
+        else newTestAgainst = TestingAgainstType.TRANSLATION;
+        return { ...state, baseTestingAgainst: newTestAgainst };
+    }),
     on(SettingsPopupAction.changeTheme, (state, payload) => ({ ...state, ...payload })),
 );
