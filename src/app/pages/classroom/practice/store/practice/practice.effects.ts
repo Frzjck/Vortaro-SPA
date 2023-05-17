9bb830dab7cdbbfc90deee95a9303dd376593760
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-17 11:22:07 +0200

    Create loadCurrentSettings effect

--- src/app/pages/classroom/practice/store/practice/practice.effects.ts
+++ src/app/pages/classroom/practice/store/practice/practice.effects.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@angular/core";
 import { createEffect, Actions, ofType, concatLatestFrom } from '@ngrx/effects';
-import { ExercisePageAction } from "./practice.actions";
 import { of } from "rxjs";
 import { Store } from "@ngrx/store";
 import { switchMap, debounceTime } from 'rxjs/operators';
+
+import { selectBaseExerciseMode, selectBaseTestingAgainst } from "@app/store/app";
+import { ExerciseContainerPageAction, ExercisePageAction } from "./practice.actions";
 import { selectCurrentWord, selectExerciseWords, selectCurrentTestingAgainst } from "./practice.reducer";
 import { _generateAnswerChoices } from "./store-utils";
 
@@ -29,17 +31,14 @@ export class ExercisesEffects {
         })
     ))
 
-    loadBaseSettings$ = createEffect(() => this.actions$.pipe(
-        ofType(ExercisePageAction.loadAnswerChoices, ExercisePageAction.nextWord),
-        debounceTime(1000),
+    loadCurrentSettings$ = createEffect(() => this.actions$.pipe(
+        ofType(ExerciseContainerPageAction.loadCurrentSettings),
         concatLatestFrom((action) => [
-            this.store.select(selectCurrentWord),
-            this.store.select(selectExerciseWords),
-            this.store.select(selectCurrentTestingAgainst),
+            this.store.select(selectBaseExerciseMode),
+            this.store.select(selectBaseTestingAgainst),
         ]),
-        switchMap(([action, currentWord, exerciseWords, testingAgainst]) => {
-            const answerChoices = _generateAnswerChoices(currentWord, exerciseWords, testingAgainst);
-            return of(ExercisePageAction.storeAnswerChoices({ answerChoices }));
+        switchMap(([action, baseExerciseMode, baseTestingAgainst]) => {
+            return of(ExerciseContainerPageAction.storeCurrentSettings({ currentExerciseMode: baseExerciseMode, currentTestingAgainst: baseTestingAgainst }));
         })
     ))
 }
