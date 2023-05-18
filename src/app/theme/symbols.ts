15d9805564c8cb77e7b6cdde881560f52877b08c
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-18 11:54:37 +0200

    Remove obsolete symbols

--- src/app/theme/symbols.ts
+++ src/app/theme/symbols.ts
@@ -1,14 +1,1 @@
-import { InjectionToken } from '@angular/core';
 
-export const THEMES = new InjectionToken<Theme[]>('THEMES');
-export const ACTIVE_THEME = new InjectionToken<string>('ACTIVE_THEME');
-
-export interface Theme {
-  name: string;
-  properties: any;
-}
-
-export interface ThemeOptions {
-  themes: Theme[];
-  active: string;
-}
