48298669bc8ec98a39738dce19aa371e02c986ef
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-18 10:55:44 +0200

    Create theme actions and delete obsolete code

--- src/app/store/app/app.actions.ts
+++ src/app/store/app/app.actions.ts
@@ -1,11 +1,14 @@
-import { createAction, props } from '@ngrx/store';
-
-export enum Types {
-    CHANGE_THEME = '[App] Change Theme',
-    TOGGLE_PIXIES = '[App] Toggle Pixies',
-}
+import { Theme } from '@app/theme/symbols';
+import { createActionGroup, props } from '@ngrx/store';
 
 
-export const togglePixies = createAction(Types.TOGGLE_PIXIES);
-
-export const changeTheme = createAction(Types.CHANGE_THEME, props<{ theme: string }>());
+export const ThemeActions = createActionGroup(
+    {
+        source: 'Theme',
+        events: {
+            "Set Theme": props<{ name: string }>(),
+            "Register Theme": props<{ theme: Theme }>(),
+            "Update Theme": props<{ name: string, properties: { [key: string]: string } }>(),
+        },
+    }
+)
