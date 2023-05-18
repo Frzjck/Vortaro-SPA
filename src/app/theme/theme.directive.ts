1881b700df8427b3834abc2d87e3de7493c94dd1
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-18 10:56:50 +0200

    Refactor directive to use ngrx store

--- src/app/theme/theme.directive.ts
+++ src/app/theme/theme.directive.ts
@@ -3,6 +3,8 @@ import { ThemeService } from './theme.service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { Theme } from './symbols';
+import { Store } from '@ngrx/store';
+import { selectActiveTheme, selectThemeProperty } from '@app/store/app';
 
 @Directive({
   selector: '[theme]'
@@ -13,18 +15,39 @@ export class ThemeDirective implements OnInit, OnDestroy {
 
   constructor(
     private _elementRef: ElementRef,
-    private _themeService: ThemeService
+    private _themeService: ThemeService,
+    private _store: Store
   ) { }
 
   ngOnInit() {
-    const active = this._themeService.getActiveTheme();
-    if (active) {
-      this.updateTheme(active);
-    }
+    // const active = this._themeService.getActiveTheme();
+    // if (active) {
+    //   this.updateTheme(active);
+    // }
 
-    this._themeService.themeChange
+    // this._themeService.themeChange
+    //   .pipe(takeUntil(this._destroy$))
+    //   .subscribe((theme: Theme) => this.updateTheme(theme));
+
+
+    this._store.select(selectActiveTheme)
       .pipe(takeUntil(this._destroy$))
       .subscribe((theme: Theme) => this.updateTheme(theme));
+
+    this._store.select(selectThemeProperty('name'))
+      .pipe(takeUntil(this._destroy$))
+      .subscribe((name: string) => {
+        // remove old theme
+        const themeClasses = this._elementRef.nativeElement.classList;
+        themeClasses.forEach(className => {
+          if (className.endsWith('-theme')) {
+            themeClasses.remove(className);
+          }
+        });
+
+        // alias element with theme name
+        themeClasses.add(`${name}-theme`);
+      });
   }
 
   ngOnDestroy() {
@@ -32,19 +55,27 @@ export class ThemeDirective implements OnInit, OnDestroy {
     this._destroy$.complete();
   }
 
+  // updateTheme(theme: Theme) {
+  //   // project properties onto the element
+  //   for (const key in theme.properties) {
+  //     this._elementRef.nativeElement.style.setProperty(key, theme.properties[key]);
+  //   }
+
+  //   // remove old theme
+  //   for (const name of this._themeService.theme) {
+  //     this._elementRef.nativeElement.classList.remove(`${name}-theme`);
+  //   }
+
+  //   // alias element with theme name
+  //   this._elementRef.nativeElement.classList.add(`${theme.name}-theme`);
+  // }
+
   updateTheme(theme: Theme) {
     // project properties onto the element
+    const elementStyle = this._elementRef.nativeElement.style;
     for (const key in theme.properties) {
-      this._elementRef.nativeElement.style.setProperty(key, theme.properties[key]);
+      elementStyle.setProperty(key, theme.properties[key]);
     }
-
-    // remove old theme
-    for (const name of this._themeService.theme) {
-      this._elementRef.nativeElement.classList.remove(`${name}-theme`);
-    }
-
-    // alias element with theme name
-    this._elementRef.nativeElement.classList.add(`${theme.name}-theme`);
   }
 
 }
