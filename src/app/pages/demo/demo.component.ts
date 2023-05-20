2e05d2bfea4736e7f1a7479198e7caed7711114b
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-20 19:32:20 +0200

    Move html and scss inside demo ts

--- src/app/pages/demo/demo.component.ts
+++ src/app/pages/demo/demo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -6,14 +6,31 @@ import { RouterModule } from '@angular/router';
   selector: 'app-demo',
   standalone: true,
   imports: [CommonModule, RouterModule],
-  templateUrl: './demo.component.html',
-  styleUrls: ['./demo.component.scss']
+  template: `
+  <div class="app-page">
+  <div class="app-page__header">
+    <h1>Demo</h1>
+  </div>
+  <div class="app-page__content">
+    <div class="navigation">
+      <a class="app-a" routerLink="./styles" routerLinkActive="app-a_active"
+        >Styles</a
+      >
+      <a class="app-a" routerLink="./shared" routerLinkActive="app-a_active"
+        >Shared</a
+      >
+    </div>
+    <router-outlet></router-outlet>
+  </div>
+  </div>
+  `,
+  styles: [`
+  .navigation {
+    padding: 20px 0;
+    }`
+  ]
 })
-export class DemoComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class DemoComponent {
 
 }
