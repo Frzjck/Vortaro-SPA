fa600b2dab0d73e4e8cbf26a91076b5b3a09125f
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-20 19:21:18 +0200

    Convert demo to standalone

--- src/app/pages/demo/demo.component.ts
+++ src/app/pages/demo/demo.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-demo',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.scss']
 })
