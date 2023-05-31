5abd5868a5bc54ab1b0d77821ef1af5a7dcc660b
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-31 18:58:41 +0200

    Set up basic functionalities to "additional translations component"

--- src/app/pages/classroom/glossary/components/word-grid/components/word-form/additional-translations/additional-translations.component.ts
+++ src/app/pages/classroom/glossary/components/word-grid/components/word-form/additional-translations/additional-translations.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormFieldComponent } from '@app/shared/controls/form-field/form-field.component';
 import { InputComponent } from '@app/shared/controls/input/input.component';
 
@@ -17,4 +17,56 @@ export class AdditionalTranslationsComponent {
 
   @Input() public values;
 
+  form: FormArray;
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+
+  ngOnInit(): void {
+    this.values = this.values ? this.values : [];
+    this.init();
+  }
+
+  ngOnDestroy(): void {
+    this.parent.removeControl(this.name);
+  }
+
+  private init(): void {
+    this.form = this.fb.array(
+      this.getFormGroupArray(this.values)
+    );
+
+    this.parent.addControl(this.name, this.form);
+  }
+
+  private getFormGroupArray(values): FormGroup[] {
+    if (!this.values.length) {
+      return [this.getFormGroup()];
+    } else {
+      return values.map(value => this.getFormGroup(value));
+    }
+  }
+
+  private getFormGroup(value?): FormGroup {
+    const group = this.fb.group({
+      translation: [null, {
+        updateOn: 'change'
+      }]
+    });
+
+    if (value) {
+      group.patchValue(value);
+    }
+
+    return group;
+  }
+
+  addTranslation(): void {
+    this.form.push(this.getFormGroup());
+  }
+
+  deleteTranslation(i: number): void {
+    this.form.removeAt(i);
+  }
 }
