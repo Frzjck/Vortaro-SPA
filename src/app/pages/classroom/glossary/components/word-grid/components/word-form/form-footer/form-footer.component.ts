9c0185eeb852b1351af1692e922c2410ff232ac7
Max Tuzenko <max.tuzenko@gmail.com>
2023-05-31 19:06:31 +0200

    Create form footer component

---
+++ src/app/pages/classroom/glossary/components/word-grid/components/word-form/form-footer/form-footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-form-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './form-footer.component.html',
+  styleUrls: ['./form-footer.component.scss']
+})
+export class FormFooterComponent {
+
+}
