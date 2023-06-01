578d2108773a1628cf1afb77762ec12a484be94f
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-01 17:03:27 +0200

    Create Subjects and observables in word form service

--- src/app/pages/classroom/glossary/components/word-grid/components/word-form/services/word-form.service.ts
+++ src/app/pages/classroom/glossary/components/word-grid/components/word-form/services/word-form.service.ts
@@ -4,5 +4,18 @@ import { Observable, Subject } from 'rxjs';
 @Injectable()
 export class WordFormService {
 
+  private addTranslation = new Subject<void>();
+  addTranslation$ = this.addTranslation.asObservable();
+
+  private addTips = new Subject<void>();
+  addTips$ = this.addTips.asObservable();
+
+  private closeForm = new Subject<void>();
+  closeForm$ = this.closeForm.asObservable();
+
+  private submitWordForm = new Subject<void>();
+  submitWordForm$ = this.submitWordForm.asObservable();
+
+
 
 }
