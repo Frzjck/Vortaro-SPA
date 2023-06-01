c7fd48b6f620b4e064eef8a10bceed95a5d8dcf5
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-01 17:05:40 +0200

    Subscribed to add translation event

--- src/app/pages/classroom/glossary/components/word-grid/components/word-form/additional-translations/additional-translations.component.ts
+++ src/app/pages/classroom/glossary/components/word-grid/components/word-form/additional-translations/additional-translations.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormFieldComponent } from '@app/shared/controls/form-field/form-field.component';
 import { InputComponent } from '@app/shared/controls/input/input.component';
+import { Subject, takeUntil } from 'rxjs';
+import { WordFormService } from '../services/word-form.service';
 
 @Component({
   selector: 'app-additional-translations',
@@ -18,18 +20,26 @@ export class AdditionalTranslationsComponent {
   @Input() public values;
 
   form: FormArray;
+
+  private destroy = new Subject<void>();
+
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private footer: WordFormService
   ) { }
 
 
   ngOnInit(): void {
     this.values = this.values ? this.values : [];
     this.init();
+    this.footer.addTranslation$.pipe(takeUntil(this.destroy)).subscribe(() => this.addTranslation())
   }
 
   ngOnDestroy(): void {
     this.parent.removeControl(this.name);
+
+    this.destroy.next();
+    this.destroy.complete();
   }
 
   private init(): void {
