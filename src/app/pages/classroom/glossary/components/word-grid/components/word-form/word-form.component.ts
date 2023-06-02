7df1711e392f64e2723c33e4cadee74aa6171cdd
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-02 14:17:25 +0200

    Remove obsolete code

--- src/app/pages/classroom/glossary/components/word-grid/components/word-form/word-form.component.ts
+++ src/app/pages/classroom/glossary/components/word-grid/components/word-form/word-form.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Word } from '@app/pages/classroom/store/words-list';
 import { FormFieldComponent } from '@app/shared/controls/form-field/form-field.component';
@@ -21,66 +21,28 @@ import { interval } from 'rxjs';
 export class WordFormComponent implements OnInit {
   @Input() word: Word;
   coreForm: FormGroup;
-  isTips = false;
   storeFormObservable$ToBeCreated = { "additionalTranslations": [] };
-  // @Output() onFinishSubmit = new EventEmitter();
+
   constructor(
   ) { }
 
-  additionalTrArray: string[];
-
   ngOnInit(): void {
-    if (this.word) {
-      this.coreForm = new FormGroup({
-        word: new FormControl(this.word.original, [Validators.required]),
-        translation: new FormControl(this.word.translation, [
-          Validators.required,
-        ]),
-        tips: new FormControl(this.word.tips),
-      });
-    } else {
-      this.coreForm = new FormGroup({
-        word: new FormControl(null, [Validators.required]),
-        translation: new FormControl(null, [Validators.required]),
-        tips: new FormControl(),
-      });
-    }
+
+    this.coreForm = new FormGroup({
+      word: new FormControl(null, [Validators.required]),
+      translation: new FormControl(null, [Validators.required]),
+    });
+    // If tips exist create form control with value
+
+    // CHECK WITH STORE IF EDITING A WORD. IF TRUE MAP WORD TO FORM AND PATCH
     interval(4000).subscribe(() => {
       console.log(this.coreForm)
     })
-  }
 
+  }
+  createTipsControl() {
+    this.coreForm.addControl("tips", new FormControl(null));
+  }
   onSubmit() {
-    // if (this.word) {
-    //   this.wordService
-    //     .editOrCreate(
-    //       this.coreForm.value.word,
-    //       this.coreForm.value.translation,
-    //       this.coreForm.value.tips,
-    //       undefined,
-    //       this.word.id,
-    //       this.formatAdditionalTr()
-    //     )
-    //     .subscribe(() => {
-    //       this.groupService.loadGroups();
-    //       this.wordService.getWordsFromServer();
-    //     });
-    // } else {
-    //   this.wordService
-    //     .editOrCreate(
-    //       this.coreForm.value.word,
-    //       this.coreForm.value.translation,
-    //       this.coreForm.value.tips,
-    //       this.group.id,
-    //       undefined,
-    //       this.formatAdditionalTr()
-    //     )
-    //     .subscribe(() => {
-    //       this.wordService.getWordsFromServer();
-    //       this.groupService.loadGroups();
-    //     });
-    // }
-    // this.additionalTrArray = undefined;
-    // this.onFinishSubmit.emit();
   }
 }
--- src/app/pages/classroom/glossary/components/word-grid/components/word-form/word-form.state.ts
+++ src/app/pages/classroom/glossary/components/word-grid/components/word-form/word-form.state.ts
@@ -1,9 +1,5 @@
 import { ComponentStore } from '@ngrx/component-store';
 import { Injectable } from '@angular/core';
-import { selectWordsByGroupId, selectWordsByIds } from '@app/pages/classroom/store/words-list';
-import { Group, selectGroups } from '@app/pages/classroom/store/groups-list';
-import { Store } from '@ngrx/store';
-import { Observable, map } from 'rxjs';
 import { FormGroup } from '@angular/forms';
 
 export interface WordFormStateModel {
