1d543cf6c46fe4018b050d8fea7ee4b4b0be1418
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-03 22:33:44 +0200

    Basic word form set up

--- src/app/pages/classroom/glossary/components/word-grid/components/word-form/word-form.component.ts
+++ src/app/pages/classroom/glossary/components/word-grid/components/word-form/word-form.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Word } from '@app/pages/classroom/store/words-list';
 import { FormFieldComponent } from '@app/shared/controls/form-field/form-field.component';
 import { InputComponent } from '@app/shared/controls/input/input.component';
@@ -9,13 +9,13 @@ import { AdditionalTranslationsComponent } from './additional-translations/addit
 import { FormFooterComponent } from './form-footer/form-footer.component';
 import { WordFormService } from './services/word-form.service';
 import { WordFormState } from './word-form.state';
-import { take } from 'rxjs';
 
 @Component({
   selector: 'app-word-form',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, FormFieldComponent, InputComponent, AdditionalTranslationsComponent, FormFooterComponent,],
   providers: [WordFormService, WordFormState],
+  // changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './word-form.component.html',
   styleUrls: ['./word-form.component.scss'],
 })
@@ -23,19 +23,30 @@ export class WordFormComponent implements OnInit {
   @Input() word: Word;
   coreForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+
+  }
 
   ngOnInit(): void {
-    this.coreForm = new FormGroup({
+    this.coreForm = this.fb.group({
       word: new FormControl(null, [Validators.required]),
       translation: new FormControl(null, [Validators.required]),
-      tips: new FormControl(null),
     });
+
+    if (this.word?.tips) this.createTipsControl()
     // CHECK WITH STORE IF EDITING A WORD. IF TRUE MAP WORD TO FORM AND PATCH
+    if (this.word) this.coreForm.patchValue({
+      word: this.word.original,
+      translation: this.word.translation,
+    })
 
     // If tips exist create form control with value
   }
 
+
+  createTipsControl() {
+    this.coreForm.addControl("tips", new FormControl(this.word.tips));
+  }
 
   onFormChanged() {
   }
