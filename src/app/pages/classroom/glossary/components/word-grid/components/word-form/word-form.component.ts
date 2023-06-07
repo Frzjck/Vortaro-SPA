50fcea46f6a9224466828d45712a3eaa6a8264bb
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-07 22:01:00 +0200

    Clean word form ts

--- src/app/pages/classroom/glossary/components/word-grid/components/word-form/word-form.component.ts
+++ src/app/pages/classroom/glossary/components/word-grid/components/word-form/word-form.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Word } from '@app/pages/classroom/store/words-list';
+import { Word, createWord } from '@app/pages/classroom/store/words-list';
 import { FormFieldComponent } from '@app/shared/controls/form-field/form-field.component';
 import { InputComponent } from '@app/shared/controls/input/input.component';
 import { AdditionalTranslationsComponent } from './additional-translations/additional-translations.component';
 import { FormFooterComponent } from './form-footer/form-footer.component';
 import { WordFormService } from './services/word-form.service';
 import { WordFormState } from './word-form.state';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-word-form',
@@ -23,23 +24,18 @@ export class WordFormComponent implements OnInit {
   @Input() word: Word;
   coreForm: FormGroup;
 
-
-  constructor(private fb: FormBuilder, private footer: WordFormService) {
-
-  }
+  constructor(private store: Store, private fb: FormBuilder, private footer: WordFormService) { }
 
   ngOnInit(): void {
+    this.footer.addTips$.subscribe(() => this.createTipsControl())
+
     this.coreForm = this.fb.group({
       word: new FormControl(null, [Validators.required]),
       translation: new FormControl(null, [Validators.required]),
     })
 
-    this.footer.addTips$.subscribe(() => {
-      this.coreForm.addControl("tips", new FormControl());
-    })
-
     if (this.word) {
-      this.coreForm.patchValue({ word: this.word.original, translation: this.word.translation, });
+      this.coreForm.patchValue({ word: this.word.original, translation: this.word.translation });
       if (this.word.tips) this.createTipsControl(this.word.tips)
     }
   }
@@ -49,9 +45,7 @@ export class WordFormComponent implements OnInit {
     this.coreForm.addControl("tips", new FormControl(value));
   }
 
-  onFormChanged() {
-  }
-
   onSubmit() {
+    this.store.dispatch(createWord(this.coreForm.value));
   }
 }
