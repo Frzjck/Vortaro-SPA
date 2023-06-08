49fc10abddb9884becaf69b3cc98875bd0b2ef8b
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-08 19:11:07 +0200

    Create create word and delete word function placeholders. Working OK

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,16 @@
 import * as functions from "firebase-functions";
 
+import { db } from "./init";
+
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
+export const onAddWordUpdateGroupWordIdArray = functions.firestore.document("users/{userId}/groups/{groupId}/words/{wordId}").onCreate(async (snap, context) => {
+    functions.logger.debug(`Running add course trigger for word id: ${context.params.wordId}`);
     response.send("Hello from Firebase!");
-});
+
+})
+
+export const onDeleteWordUpdateGroupWordIdArray = functions.firestore.document("users/{userId}/groups/{groupId}/words/{wordId}").onDelete(async (snap, context) => {
+    functions.logger.debug(`Running delete course trigger for word id: ${context.params.wordId}`)
+})
