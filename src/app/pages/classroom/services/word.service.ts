874804aecac8bbe219dbd2bd5242062effceb67d
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-10 11:56:24 +0200

    Remove obsolete

--- src/app/pages/classroom/services/word.service.ts
+++ src/app/pages/classroom/services/word.service.ts
@@ -18,7 +18,6 @@ export class WordService {
   constructor(private db: AngularFirestore) { }
 
   getWordsFromServer(userId) {
-    console.log("------>>>>> getWordsFromServer ", userId)
     const userRef = this.db.collection("users").doc(userId).ref;
     // Purpose of next line is to extract all collections named "word" located inside a certain user. We order by __name__ (internal fire id, alternatively firebase.firestore.FieldPath.documentId()), as far as I understand because user has no other fields (must be a different reason since substitution with __createTime__ throws an error).
     // https://stackoverflow.com/questions/68049541/collectiongroupquery-but-limit-search-to-subcollections-under-a-particular-docum/68049847#68049847
