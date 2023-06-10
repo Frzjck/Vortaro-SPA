283500d8b37ea77792df66cdb838a288b03526df
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-10 11:34:01 +0200

    Create function to add words to db

--- src/app/pages/classroom/services/word.service.ts
+++ src/app/pages/classroom/services/word.service.ts
@@ -5,7 +5,8 @@ import { convertSnaps } from '../../../shared/utils/db-utils';
 
 
 import { Word } from '@app/pages/classroom/store/words-list';
-import { Store } from '@ngrx/store';
+import { from } from 'rxjs';
+import { FireWordCreateRequest } from '@app/models/backend/word';
 
 @Injectable({
   providedIn: 'root',
@@ -14,12 +15,17 @@ export class WordService {
 
   exerciseMode: string;
 
-  constructor(private db: AngularFirestore, private store: Store) { }
+  constructor(private db: AngularFirestore) { }
+
   getWordsFromServer(userId) {
     console.log("------>>>>> getWordsFromServer ", userId)
     const userRef = this.db.collection("users").doc(userId).ref;
     // Purpose of next line is to extract all collections named "word" located inside a certain user. We order by __name__ (internal fire id, alternatively firebase.firestore.FieldPath.documentId()), as far as I understand because user has no other fields (must be a different reason since substitution with __createTime__ throws an error).
     // https://stackoverflow.com/questions/68049541/collectiongroupquery-but-limit-search-to-subcollections-under-a-particular-docum/68049847#68049847
     return this.db.collectionGroup('words', (ref) => ref.orderBy("__name__").startAt(userRef.path).endAt(userRef.path + "\uf8ff")).get().pipe(map((results) => convertSnaps<Word>(results)));
+  }
+
+  addWordRequest(word: FireWordCreateRequest, groupId: string, userId: string) {
+    return from(this.db.collection(`/users/${userId}/groups/${groupId}/words`).add(word))
   }
 }
