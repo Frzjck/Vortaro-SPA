417d3814bb335683c8f94e3eaef9acf5e25cdc5f
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-13 15:47:09 +0200

    Remove Obsolete

--- src/app/pages/classroom/classroom-routing.module.ts
+++ src/app/pages/classroom/classroom-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { GlossaryComponent } from './glossary/glossary.component';
-
 const routes: Routes = [{
   path: 'my-vocabulary',
-  component: GlossaryComponent,
   loadChildren: () => import('@glossary/glossary-routing.module')
 },
 // If we add here ↓ component property, it will be able to render inside its <router-outlet> components located in loadChildren routes
