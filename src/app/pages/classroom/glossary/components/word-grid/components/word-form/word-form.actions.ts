2c60492bbf4c8ae68e1cc83bd8d14bb55d031ebd
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-15 13:50:36 +0200

    Create word form update actions

--- src/app/pages/classroom/glossary/components/word-grid/components/word-form/word-form.actions.ts
+++ src/app/pages/classroom/glossary/components/word-grid/components/word-form/word-form.actions.ts
@@ -9,7 +9,8 @@ export const WordFormAction = createActionGroup(
     {
         source: 'Word Form',
         events: {
-            "Submit Word Form": props<{ word: FormWord, groupId: string }>(),
+            "Create Word": props<{ word: FormWord, groupId: string }>(),
+            "Update Word": props<{ word: FormWord, groupId: string, wordId: string }>(),
 
             "Cancel New Word Mode": emptyProps(),
             "Cancel Edit Word": emptyProps(),
@@ -23,6 +24,9 @@ export const WordFormAPIAction = createActionGroup(
         events: {
             "Create Word Success": props<{ groupId: string, word: Word }>(),
             "Create Word Error": props<{ error: string }>(),
+
+            "Update Word Success": props<{ id: string, changes: Partial<Word> }>(),
+            "Update Word Error": props<{ error: string }>(),
         },
     }
 )
