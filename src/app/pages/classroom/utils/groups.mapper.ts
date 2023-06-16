2f04818c5a49153779981d0ac23780c33cb31104
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-16 21:13:18 +0200

    Create groups mapper util

---
+++ src/app/pages/classroom/utils/groups.mapper.ts
@@ -0,0 +1,29 @@
+import firebase from "firebase/compat/app";
+
+import { FireGroupCreateRequest, FireGroupUpdateRequest } from "@app/models/backend/group";
+import { FormGroup } from "../glossary/components/group-form/models";
+import { Group } from "../store/groups-list/groups.models";
+
+export const formGroupToNewFireGroup = (group: FormGroup): FireGroupCreateRequest => {
+    return {
+        ...group,
+        wordIds: [],
+        created: firebase.firestore.FieldValue.serverTimestamp(),
+    };
+};
+export const formGroupToNewGroup = (group: FormGroup, id: string): Group => {
+    return {
+        ...group,
+        wordIds: [],
+        averageProficiency: 1,
+        id
+    };
+};
+
+export const formGroupToUpdatedFireGroup = (group: FormGroup): FireGroupUpdateRequest => {
+    return {
+        ...group,
+        wordIds: [],
+        updated: firebase.firestore.FieldValue.serverTimestamp()
+    };
+};
