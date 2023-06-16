a62a8a397ead5a1eb2b82d029d10c2c3ec015ab6
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-16 14:16:49 +0200

    Set up fire function onDeleteWordUpdateGroupWordIdArray

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,5 +19,13 @@ export const onAddWordUpdateGroupWordIdArray = functions.firestore.document("use
 })
 
 export const onDeleteWordUpdateGroupWordIdArray = functions.firestore.document("users/{userId}/groups/{groupId}/words/{wordId}").onDelete(async (snap, context) => {
-    functions.logger.debug(`Running delete course trigger for word id: ${context.params.wordId}`)
+    functions.logger.debug(`Running delete course trigger for word id: ${context.params.wordId}`);
+
+    return db.runTransaction(async (transaction) => {
+        const groupRef = db.doc(`users/${context.params.userId}/groups/${context.params.groupId}`);
+        const groupSnap = await transaction.get(groupRef);
+        const group = groupSnap.data();
+        group.wordIds = group.wordIds.filter(id => id !== context.params.wordId);
+        transaction.set(groupRef, group);
+    });
 })
