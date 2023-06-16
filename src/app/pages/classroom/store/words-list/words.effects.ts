788bfc4cfaa8c9994481af4e7468d82ef0250b76
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-16 13:41:24 +0200

    Create word delete effect

--- src/app/pages/classroom/store/words-list/words.effects.ts
+++ src/app/pages/classroom/store/words-list/words.effects.ts
@@ -1,28 +1,24 @@
 import { Injectable } from '@angular/core';
 import { createEffect, Actions, ofType, concatLatestFrom, OnInitEffects } from '@ngrx/effects';
 
-// import * as firestore from "@google-cloud/firestore";
-import firebase from "firebase/compat/app";
-
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-
-import { from, of } from 'rxjs';
+import { of } from 'rxjs';
 import { map, switchMap, catchError, take, tap } from 'rxjs/operators';
 
-import { UnknownPageWordAction } from './words.actions';
+import { UnknownPageWordAction, WordAPIResponseAction } from './words.actions';
 import { Word } from './words.models';
 import { WordService } from '@app/pages/classroom/services/word.service';
 import { Store } from '@ngrx/store';
 import { formWordToNewFireWord, formWordToNewWord, formWordToUpdatedFireWord, formWordToUpdatedWord } from '../../utils/words.mapper';
 import { selectUserId } from '@app/store/user/user.selectors';
-import { WordFormAPIAction, WordFormAction } from '@glossary/components/word-grid/components/word-form/word-form.actions';
+import { WordFormAction } from '@glossary/components/word-grid/components/word-form/word-form.actions';
+import { GlossaryWordUIAction } from '../../glossary/components/word-grid/components/word-ui/word-ui.actions';
+
 
 
 @Injectable()
 export class WordsEffects {
     constructor(
         private actions$: Actions,
-        private afs: AngularFirestore,
         private wordService: WordService,
         private store: Store,
     ) { }
@@ -35,8 +31,8 @@ export class WordsEffects {
         switchMap(([, uid]) => {
             return this.wordService.getWordsFromServer(uid).pipe(
                 take(1),
-                map((words: Word[]) => UnknownPageWordAction.readWordsSuccess({ words })),
-                catchError(err => of(UnknownPageWordAction.readWordsError(err.message)))
+                map((words: Word[]) => WordAPIResponseAction.readWordsSuccess({ words })),
+                catchError(err => of(WordAPIResponseAction.readWordsError(err.message)))
             )
         }
         )
@@ -49,10 +45,10 @@ export class WordsEffects {
             this.store.select(selectUserId),
         ]),
         switchMap(([{ formWord, groupId }, fireWord, userId]) =>
-            this.wordService.addWordRequest(fireWord, groupId, userId).pipe(
+            this.wordService.addWordRequest(fireWord, userId, groupId).pipe(
                 map(res => (formWordToNewWord(formWord, res.id))),
-                map((word: Word) => WordFormAPIAction.createWordSuccess({ groupId, word })),
-                catchError(err => of(WordFormAPIAction.createWordError(err.message)))
+                map((word: Word) => WordAPIResponseAction.createWordSuccess({ groupId, word })),
+                catchError(err => of(WordAPIResponseAction.createWordError(err.message)))
             )
         )
     ));
@@ -64,21 +60,23 @@ export class WordsEffects {
             this.store.select(selectUserId),
         ]),
         switchMap(([{ formWord, groupId, wordId }, fireWord, userId]) =>
-            this.wordService.updateWordRequest(fireWord, groupId, userId, wordId).pipe(
-                map(() => WordFormAPIAction.updateWordSuccess({ id: wordId, changes: formWordToUpdatedWord(formWord) })
+            this.wordService.updateWordRequest(fireWord, userId, groupId, wordId).pipe(
+                map(() => WordAPIResponseAction.updateWordSuccess({ wordId, changes: formWordToUpdatedWord(formWord) })
                 ),
-                catchError(err => of(UnknownPageWordAction.updateWordError(err.message)))
+                catchError(err => of(WordAPIResponseAction.updateWordError(err.message)))
             )
         )
     ));
 
     delete$ = createEffect(() => this.actions$.pipe(
-        ofType(UnknownPageWordAction.deleteWord),
-        map((action) => action.id),
-        switchMap(id =>
-            from(this.afs.collection('words').doc(id).delete()).pipe(
-                map(() => UnknownPageWordAction.deleteWordSuccess({ id })),
-                catchError(err => of(UnknownPageWordAction.deleteWordError(err.message)))
+        ofType(GlossaryWordUIAction.deleteWord),
+        concatLatestFrom(() => [
+            this.store.select(selectUserId),
+        ]),
+        switchMap(([{ wordId, groupId }, userId]) =>
+            this.wordService.deleteWordRequest(userId, groupId, wordId).pipe(
+                map(() => WordAPIResponseAction.deleteWordSuccess({ wordId })),
+                catchError(err => of(WordAPIResponseAction.deleteWordError(err.message)))
             )
         )
     ));
