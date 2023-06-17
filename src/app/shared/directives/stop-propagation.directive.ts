cd4d0f701a9a7de3e1c9647869b39a568361e1c0
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-17 12:46:29 +0200

    Improve StopPropagationDirective and make it standalone

--- src/app/shared/directives/stop-propagation.directive.ts
+++ src/app/shared/directives/stop-propagation.directive.ts
@@ -2,10 +2,19 @@ import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appStopPropagation]',
+  standalone: true
 })
 export class StopPropagationDirective {
+  @HostListener('keydown', ['$event'])
+  public onKeyDown(event: KeyboardEvent): void {
+    const key = event.key;
+    if (key === 'Enter' || key === ' ') {
+      event.stopPropagation();
+    }
+  }
+
   @HostListener('click', ['$event'])
-  public onClick(event: any): void {
-    event.stopPropagation();
+  public onClick(event: MouseEvent): void {
+    event.stopImmediatePropagation();
   }
 }
