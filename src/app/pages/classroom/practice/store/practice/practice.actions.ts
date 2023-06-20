94153d5aff19ea2ae6927bc1dc89e75222c4fe45
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-20 20:44:37 +0200

    Delete Obsolete

--- src/app/pages/classroom/practice/store/practice/practice.actions.ts
+++ src/app/pages/classroom/practice/store/practice/practice.actions.ts
@@ -3,21 +3,6 @@ import { ExerciseModeType, TestingAgainstType } from '@app/store/app/app.reducer
 import { createActionGroup, emptyProps, props } from '@ngrx/store';
 
 
-export const ExerciseContainerPageAction = createActionGroup(
-    {
-        source: 'Exercise Page',
-        events: {
-            Enter: emptyProps(),
-            "Reset Exercise State": emptyProps(),
-            "Exercise Finished": props<{ score: number }>(),
-            "Load Current Settings": emptyProps(),
-            "Store Current Settings": props<{
-                currentExerciseMode: ExerciseModeType,
-                currentTestingAgainst: TestingAgainstType
-            }>(),
-        },
-    }
-)
 export const ExercisePageAction = createActionGroup(
     {
         source: 'Exercise Page',
