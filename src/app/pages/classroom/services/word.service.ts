8bddfb0a009570c9ad32a534b6f5bf24b5a0c54b
Max Tuzenko <max.tuzenko@gmail.com>
2023-06-30 20:54:23 +0200

    Bugfix getWordsFromServer

--- src/app/pages/classroom/services/word.service.ts
+++ src/app/pages/classroom/services/word.service.ts
@@ -14,13 +14,15 @@ export class WordService {
   firestore = inject(Firestore);
 
 
-  getWordsFromServer(userId) {
-    const userRef = doc(this.firestore, `/users/${userId}`);
 
+
+  getWordsFromServer(userId) {
     // Purpose of next line is to extract all collections named "word" located inside a certain user. We order by __name__ (internal fire id, alternatively firebase.firestore.FieldPath.documentId()), as far as I understand because user has no other fields (must be a different reason since substitution with __createTime__ throws an error).
     // https://stackoverflow.com/questions/68049541/collectiongroupquery-but-limit-search-to-subcollections-under-a-particular-docum/68049847#68049847
-    const wordsCollectionGroup = collectionGroup(this.firestore, 'words');
+    // return this.db.collectionGroup('words', (ref) => ref.orderBy("__name__").startAt(userRef.path).endAt(userRef.path + "\uf8ff")).get().pipe(map((results) => convertSnaps<Word>(results)));
 
+    const userRef = doc(this.firestore, `/users/${userId}`);
+    const wordsCollectionGroup = collectionGroup(this.firestore, 'words');
     const queryRef = query(
       wordsCollectionGroup,
       orderBy('__name__'),
@@ -28,13 +30,7 @@ export class WordService {
       endAt(userRef.path + '\uf8ff')
     );
 
-    return from(getDocs(queryRef)).pipe(
-      tap((res) => console.log("▄▄▄ ▄ ▄▄▄", res)),
-      map(
-        (res) => res.docs.map((doc) => doc.data()) as Word[])
-    );
-
-    // return this.db.collectionGroup('words', (ref) => ref.orderBy("__name__").startAt(userRef.path).endAt(userRef.path + "\uf8ff")).get().pipe(map((results) => convertSnaps<Word>(results)));
+    return from(getDocs(queryRef)).pipe(map((res) => convertSnaps<Word>(res)));
   }
 
   addWordRequest(word: FireWordCreateRequest, userId: string, groupId: string) {
